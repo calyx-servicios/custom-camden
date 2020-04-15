67302a13eb4726e6fbf9f39b1bf425fbb4d4a15f
FedericoGregori <jfedericogregori@gmail.com>
2020-04-14 21:51:34 -0300

    [ADD][camden_travel] Projects/Travels Smart Button in Partners

--- camden_travel/models/partner.py
+++ camden_travel/models/partner.py
@@ -5,11 +5,21 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
+
 class Partner(models.Model):
     _inherit = "res.partner"
-
 
     age = fields.Integer(string='Age')
     passport = fields.Integer(string='Passport')
     passport_expiration = fields.Date(string='Passport expiration')
     travel_date = fields.Date(string='Travel date')
+
+    # Assigned Projects/Travels Smart Button
+    num_other_projects = fields.Integer(
+        compute='_compute_num_other_projects')
+
+    def _compute_num_other_projects(self):
+        for partner in self:
+            domain = [
+                ('partner_id.id', '=', partner.id)]
+            partner.num_other_projects = self.env['project.project'].search_count(domain)
